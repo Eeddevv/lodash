f7b38e6535c32fbe0fcabd1e9bfd2184521c13c6
eddevv <edddevvv@gmail.com>
2025-05-11 10:07:09 +0300

    fix:drop, add dropRight

--- dropRight/index.js
+++ dropRight/index.js
@@ -1,16 +1,12 @@
-function drop(arr, num = 1) {
+function dropRight(arr, num = 1) {
   if (Array.isArray(arr) && arr.length > 0) {
     if (num && typeof num === 'number') {
       if (num > arr.length) return []
 
       let result = []
       
-      for (let index = 0; index < arr.length; index++) {
-        if (index + 1 === num) {
-          result.length = 0
-        } else {
-          result = [...result, arr[index]]
-        }
+      for (let index = 0; index < arr.length - num; index++) {
+        result = [...result, arr[index]]
       }
 
       return result
